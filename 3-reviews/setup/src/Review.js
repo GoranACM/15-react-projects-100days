e2dbc42dbd224ac2b6659688e5634065446d277f
GoranACM <goranacm@gmail.com>
2020-11-01 17:52:26 +1100

    Random reviews and json tweak

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -6,18 +6,36 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
-      return newIndex;
+      return checkNumber(newIndex);
     });
   };
 
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index - 1;
-      return newIndex;
+      return checkNumber(newIndex);
     });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
   };
 
   return (
@@ -39,7 +57,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className='random-btn'>random review</button>
+      <button className='random-btn' onClick={randomPerson}>
+        random review
+      </button>
     </article>
   );
 };
