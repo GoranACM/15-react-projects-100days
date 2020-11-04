c2c78304a9f14322ee7fccdefdf6966e315187be
GoranACM <goranacm@gmail.com>
2020-11-04 19:01:27 +1100

    Autoslide of slider

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+import { cleanup } from '@testing-library/react';
 
 function App() {
   const [people, setPeople] = useState(data);
@@ -16,6 +17,15 @@ function App() {
       setIndex(0);
     }
   }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
 
   return (
     <section className='section'>
