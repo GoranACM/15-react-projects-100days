4d24cb26e9aab3949dcf4032b609d274b68fcb80
GoranACM <goranacm@gmail.com>
2020-11-05 20:13:47 +1100

    Color values and %

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
-
-  return <h4>single color</h4>;
+  const bcg = rgb.join(',');
+  // const hex = rgbToHex(...rgb);
+  return (
+    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexColor}</p>
+    </article>
+  );
 };
 
 export default SingleColor;
