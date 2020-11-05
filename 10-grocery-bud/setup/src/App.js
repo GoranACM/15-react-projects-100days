1c4b007782adcf0b594b1542ff9e62da3cd29d4c
GoranACM <goranacm@gmail.com>
2020-11-05 21:36:31 +1100

    Alert danger

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -13,6 +13,9 @@ function App() {
     e.preventDefault();
     if (!name) {
       // alert display
+      //setAlert({ show: true, msg: 'please enter value', type: 'danger' });
+
+      showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
       // setup edit
     } else {
@@ -22,10 +25,15 @@ function App() {
       setName('');
     }
   };
+
+  const showAlert = (show: false, type: '', msg: '') => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className='form-control'>
           <input
@@ -39,10 +47,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className='grocery-container'>
-        <List items={list} />
-        <button className='clear-btn'>clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
     </section>
   );
 }
