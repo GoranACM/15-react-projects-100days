177847c0de2a8a21f31d6528d2c3a9de7a5d3e9c
GoranACM <goranacm@gmail.com>
2020-11-08 22:51:08 +1100

    Focus input

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -6,13 +6,20 @@ const SearchForm = () => {
 
   const searchValue = React.useRef('');
 
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className='search section'>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your fav cocktail</label>
           <input
