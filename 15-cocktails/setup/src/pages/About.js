e9a07b3782b7204dde68f1b5247ecd009e0ebff7
GoranACM <goranacm@gmail.com>
2020-11-08 22:03:02 +1100

    About and Error pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+        corporis quo modi quibusdam ratione? Ad vel repellat officiis eius
+        voluptas nostrum ab ea ex distinctio explicabo sunt deleniti, unde saepe
+        impedit quis asperiores quo ut neque non cum magni? Possimus.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
